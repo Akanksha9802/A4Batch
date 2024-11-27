1840d3c05e22253eca2ca7651868fff84e95303e
AKANKSHA <AKANKSHA@DESKTOP-FPN633R>
2024-11-27 13:55:48 +0530

    1st push from project

---
+++ HelloWorld/src/program/pattern1.java
@@ -0,0 +1,19 @@
+package program;
+
+public class pattern1 {
+public static void main(String[]args)
+{
+	char ch='A';
+	for(int i= 1;i<=5;i++) 
+	{
+		for(int j=1;j<=3;j++) 
+		{
+			System.out.print(ch+ " ");
+			ch++;
+		}
+		System.out.println();
+	}
+	
+}
+
+}
